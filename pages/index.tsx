095f8d57fa71863fe2491cb988b6b7c2aa0af5ae
tlylt <liu_yongliang@hotmail.com>
2021-10-01 11:32:26 +0800

    Initial update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,25 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
+import items from '../modstack.json'
 export const Home = (): JSX.Element => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>ModStack</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.tsx</code>
-      </p>
-
-      <button
-        onClick={() => {
-          window.alert('With typescript and Jest')
-        }}
-      >
-        Test Button
-      </button>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
+      <h1 className="title">SOC Module Tech Stack</h1>
+      <p className="description">Search</p>
+      {items.map((item, index) => (
+        <div className="card" key={index}>
+          <h3>{item.moduleCode}</h3>
+          <p>{item.moduleName}</p>
+          {item.stack.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </div>
+      ))}
     </main>
-
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
-      </a>
-    </footer>
+    <footer></footer>
 
     <style jsx>{`
       .container {
@@ -135,29 +91,11 @@ export const Home = (): JSX.Element => (
         font-size: 1.5rem;
       }
 
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
       .card {
         margin: 1rem;
         flex-basis: 45%;
         padding: 1.5rem;
+        width: 20rem;
         text-align: left;
         color: inherit;
         text-decoration: none;
