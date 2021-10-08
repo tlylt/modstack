1cbc9855ec3bef13fb83af4588bc185e16382f00
tlylt <liu_yongliang@hotmail.com>
2021-10-08 20:28:21 +0800

    add link

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,7 +10,27 @@ export interface Module {
 export default function Card({ module }: { module: Module }): JSX.Element {
   return (
     <div className="w-64 border p-4 shadow-sm rounded hover:border-yellow-700">
-      <h3 className="font-bold">{module.moduleCode}</h3>
+      <div className="flex gap-2">
+        <h3 className="font-bold">{module.moduleCode}</h3>
+        <a href={module.link} className="" target="_blank" rel="noreferrer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#597e8d"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="cursor-pointer"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5" />
+            <path d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5" />
+          </svg>
+        </a>
+      </div>
       <p>{module.moduleName}</p>
       <hr className="my-2" />
       {module.stack.map((module) => (
